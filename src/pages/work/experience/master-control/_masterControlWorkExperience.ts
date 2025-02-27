3b1d2d86cfeb31ea73fe2b8501db17baaeba3c16
Andrew Holbrook <holz55@gmail.com>
2025-02-27 01:37:40 -0700

    run prettier

--- src/pages/work/experience/master-control/_masterControlWorkExperience.ts
+++ src/pages/work/experience/master-control/_masterControlWorkExperience.ts
@@ -4,7 +4,8 @@ export const masterControlWorkExperience: WorkExperience = {
   company: "MasterControl",
   title: "Software Engineer II",
   period: "2014 - Feb 2018",
-  description: "Individual contributor taking on a variety of software development tasks.",
+  description:
+    "Individual contributor taking on a variety of software development tasks.",
   highlights: [
     "Received Innovation Award for the QuickNav feature, significantly reducing task completion time.",
     "Designed stateful UI architecture using ngrx/store, optimizing user experience and system performance.",
