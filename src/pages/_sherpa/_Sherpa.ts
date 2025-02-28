532fa0d37bd0f65ef943cb3a357c85faddfd9157
Andrew Holbrook <holz55@gmail.com>
2025-02-28 13:50:30 -0700

    feat: Add ThemeSherpa for centralized theme navigation and routing

--- src/pages/_sherpa/_Sherpa.ts
+++ src/pages/_sherpa/_Sherpa.ts
@@ -4,21 +4,26 @@ import { makePlaySherpa, type PlaySherpa } from "./_PlaySherpa";
 import { makeWorkSherpa, type WorkSherpa } from "./_WorkSherpa";
 import { makeFamilySherpa, type FamilySherpa } from "./_FamilySherpa";
 import { makeResourcesSherpa, type ResourcesSherpa } from "./_ResourcesSherpa";
+import { makeThemeSherpa, type ThemeSherpa } from "./_ThemeSherpa";
 
-interface Sherpa {
-  family: FamilySherpa;
-  home: LinkResolver;
-  play: PlaySherpa;
-  resources: ResourcesSherpa;
+export interface Sherpa {
+  home: () => string;
   work: WorkSherpa;
+  family: FamilySherpa;
+  play: PlaySherpa;
+  theme: ThemeSherpa;
+  resources: ResourcesSherpa;
 }
 
-export const makeSherpa = (
-  config: SherpaConfig = defaultSherpaConfig
-): Sherpa => ({
-  family: makeFamilySherpa("/family", config),
-  home: (props) => makeLinkResolver("/", props, config),
-  play: makePlaySherpa("/play", config),
-  resources: makeResourcesSherpa("/", config),
-  work: makeWorkSherpa("/work", config),
-});
+export function makeSherpa(config: SherpaConfig = defaultSherpaConfig): Sherpa {
+  return {
+    home: () => '/',
+    work: makeWorkSherpa('/work', config),
+    family: makeFamilySherpa('/family', config),
+    play: makePlaySherpa('/play', config),
+    theme: makeThemeSherpa('/theme', config),
+    resources: makeResourcesSherpa('/', config),
+  };
+}
+
+export type { ThemeSherpa } from './_ThemeSherpa';
---
+++ src/pages/_sherpa/_ThemeSherpa.ts
@@ -0,0 +1,23 @@
+import { type SherpaConfig } from "./_util/_SherpaConfig";
+
+export interface ThemeSherpa {
+  index: () => string;
+  colors: () => string;
+  typography: () => string;
+  spacing: () => string;
+  borderRadius: () => string;
+  shadows: () => string;
+  transitions: () => string;
+}
+
+export function makeThemeSherpa(basePath: string, config: SherpaConfig): ThemeSherpa {
+  return {
+    index: () => basePath,
+    colors: () => `${basePath}/colors`,
+    typography: () => `${basePath}/typography`,
+    spacing: () => `${basePath}/spacing`,
+    borderRadius: () => `${basePath}/border-radius`,
+    shadows: () => `${basePath}/shadows`,
+    transitions: () => `${basePath}/transitions`,
+  };
+} 
