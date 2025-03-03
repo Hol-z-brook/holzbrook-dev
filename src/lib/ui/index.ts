c001037c32d5fee46249d454e7a470ab8ceda2e0
Andrew Holbrook <holz55@gmail.com>
2025-03-03 16:51:22 -0700

    refactor: Remove lists module and replace with native HTML elements

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -2,7 +2,6 @@ export * from './backgrounds';
 export * from './breadcrumbs';
 export * from './buttons';
 export * from './layouts';
-export * from './lists';
 export * from './navigation';
 export * from './pages';
 export * from './typography';
--- src/lib/ui/lists/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Ul } from './Ul.astro';
-export { default as Ol } from './Ol.astro';
-export { default as Li } from './Li.astro';
