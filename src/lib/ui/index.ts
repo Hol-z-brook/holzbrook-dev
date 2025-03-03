9dcc8f8650f2b34ae15bb1e70205739284b8a628
Andrew Holbrook <holz55@gmail.com>
2025-03-03 16:09:40 -0700

    refactor: Standardize UI component imports across project files

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,119 +1,48 @@
 // Typography
-import H1 from './typography/H1.astro';
-import H2 from './typography/H2.astro';
-import H3 from './typography/H3.astro';
-import H4 from './typography/H4.astro';
-import H5 from './typography/H5.astro';
-import H6 from './typography/H6.astro';
-import P from './typography/P.astro';
-import Strong from './typography/Strong.astro';
-import Em from './typography/Em.astro';
-import Blockquote from './typography/Blockquote.astro';
-import Code from './typography/Code.astro';
-import Pre from './typography/Pre.astro';
+export { default as H1 } from './typography/H1.astro';
+export { default as H2 } from './typography/H2.astro';
+export { default as H3 } from './typography/H3.astro';
+export { default as H4 } from './typography/H4.astro';
+export { default as H5 } from './typography/H5.astro';
+export { default as H6 } from './typography/H6.astro';
+export { default as P } from './typography/P.astro';
+export { default as Strong } from './typography/Strong.astro';
+export { default as Em } from './typography/Em.astro';
+export { default as Blockquote } from './typography/Blockquote.astro';
+export { default as Code } from './typography/Code.astro';
+export { default as Pre } from './typography/Pre.astro';
 
 // Layout
-import Box from './layout/Box.astro';
-import Stack from './layout/Stack.astro';
-import HStack from './layout/HStack.astro';
-import VStack from './layout/VStack.astro';
-import Center from './layout/Center.astro';
+export { default as Box } from './layout/Box.astro';
+export { default as Stack } from './layout/Stack.astro';
+export { default as HStack } from './layout/HStack.astro';
+export { default as VStack } from './layout/VStack.astro';
+export { default as Center } from './layout/Center.astro';
 
 // Buttons
-import PrimaryButton from './button/PrimaryButton.astro';
-import OutlinedButton from './button/OutlinedButton.astro';
+export { default as PrimaryButton } from './button/PrimaryButton.astro';
+export { default as OutlinedButton } from './button/OutlinedButton.astro';
 
 // Page Header
-import PageHeader from './page-header/PageHeader.astro';
-import PageHeaderTitle from './page-header/PageHeaderTitle.astro';
-import PageHeaderBody from './page-header/PageHeaderBody.astro';
-import PageHeaderSubtitle from './page-header/PageHeaderSubtitle.astro';
+export { default as PageHeader } from './page-header/PageHeader.astro';
+export { default as PageHeaderTitle } from './page-header/PageHeaderTitle.astro';
+export { default as PageHeaderBody } from './page-header/PageHeaderBody.astro';
+export { default as PageHeaderSubtitle } from './page-header/PageHeaderSubtitle.astro';
 
 // Lists
-import Ul from './list/Ul.astro';
-import Ol from './list/Ol.astro';
-import Li from './list/Li.astro';
+export { default as Ul } from './list/Ul.astro';
+export { default as Ol } from './list/Ol.astro';
+export { default as Li } from './list/Li.astro';
 
 // Navigation
-import Link from './navigation/Link.astro';
-import Navigation from './navigation/Navigation.astro';
-import BackLink from './navigation/BackLink.astro';
+export { default as Link } from './navigation/Link.astro';
+export { default as Navigation } from './navigation/Navigation.astro';
+export { default as BackLink } from './navigation/BackLink.astro';
 
 // Breadcrumbs
-import BreadcrumbItem from './breadcrumb/BreadcrumbItem.astro';
-import BreadcrumbList from './breadcrumb/BreadcrumbList.astro';
+export { default as BreadcrumbItem } from './breadcrumb/BreadcrumbItem.astro';
+export { default as BreadcrumbList } from './breadcrumb/BreadcrumbList.astro';
 
 // Other
-import Card from './Card.astro';
-import GradientBackground from './GradientBackground.astro';
-
-export default {
-  // Typography
-  Typography: {
-    Heading: {
-      H1,
-      H2,
-      H3,
-      H4,
-      H5,
-      H6,
-    },
-    Text: {
-      P,
-      Strong,
-      Em,
-      Blockquote,
-      Code,
-      Pre,
-    },
-  },
-
-  // Layout
-  Layout: {
-    Box,
-    Flex: {
-      Stack,
-      HStack,
-      VStack,
-      Center,
-    },
-  },
-
-  // Buttons
-  Button: {
-    Primary: PrimaryButton,
-    Outlined: OutlinedButton,
-  },
-
-  // Page Header
-  PageHeader: {
-    Root: PageHeader,
-    Title: PageHeaderTitle,
-    Body: PageHeaderBody,
-    Subtitle: PageHeaderSubtitle,
-  },
-
-  // Lists
-  List: {
-    Ul,
-    Ol,
-    Li,
-  },
-
-  // Navigation
-  Navigation: {
-    Link,
-    Root: Navigation,
-    BackLink,
-  },
-
-  // Breadcrumbs
-  Breadcrumb: {
-    Item: BreadcrumbItem,
-    List: BreadcrumbList,
-  },
-
-  // Other
-  Card,
-  GradientBackground,
-};
+export { default as Card } from './Card.astro';
+export { default as GradientBackground } from './GradientBackground.astro';
