7c577e2144ab0cde3da6a202c1780ac40984eaa8
Andrew Holbrook <holz55@gmail.com>
2025-03-03 13:14:51 -0700

    refactor: Migrate WorkPage components to new _ui module with consistent structure

---
+++ src/pages/work/_ui/PageSection/index.ts
@@ -0,0 +1,9 @@
+import PageSection from "./PageSection.astro";
+import PageSectionGrid from "./PageSectionGrid.astro";
+import PageSectionTitle from "./PageSectionTitle.astro";
+
+export default {
+  Container: PageSection,
+  Grid: PageSectionGrid,
+  Title: PageSectionTitle,
+};
---
+++ src/pages/work/_ui/index.ts
@@ -0,0 +1,17 @@
+import Page from "./Page.astro";
+import PageSection from "./PageSection";
+import ExperienceCard from "./ExperienceCard.astro";
+import ProjectCard from "./ProjectCard.astro";
+import SkillCard from "./SkillCard.astro";
+import SkillCategoryCard from "./SkillCategoryCard.astro";
+import ResumeSkeleton from "./ResumeSkeleton.astro";
+
+export default {
+  Page: Page,
+  PageSection: PageSection,
+  ExperienceCard: ExperienceCard,
+  ProjectCard: ProjectCard,
+  SkillCard: SkillCard,
+  SkillCategoryCard: SkillCategoryCard,
+  ResumeSkeleton: ResumeSkeleton,
+};
