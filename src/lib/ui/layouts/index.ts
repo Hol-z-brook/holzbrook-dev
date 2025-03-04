7724f68bcc49d7103b309dccfc0e13d51a841e0a
Andrew Holbrook <holz55@gmail.com>
2025-03-03 17:08:57 -0700

    refactor: Remove Box component and replace with native div elements

--- src/lib/ui/layouts/index.ts
+++ src/lib/ui/layouts/index.ts
@@ -1,4 +1,3 @@
-export { default as Box } from './Box.astro';
 export { default as Stack } from './Stack.astro';
 export { default as HStack } from './HStack.astro';
 export { default as VStack } from './VStack.astro';
