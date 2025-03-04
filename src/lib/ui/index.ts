23e7b8afbc4b37b10a558a04a1d81187637a7792
Andrew Holbrook <holz55@gmail.com>
2025-03-03 17:44:32 -0700

    reintroduce <Li, <Ol, <Ul

--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -3,6 +3,7 @@ export * from './breadcrumbs';
 export * from './buttons';
 export * from './heroes';
 export * from './layouts';
+export * from './lists';
 export * from './navigation';
 export * from './pages';
 export * from './typography';
---
+++ src/lib/ui/lists/index.ts
@@ -0,0 +1,3 @@
+export { default as Li } from './Li.astro';
+export { default as Ol } from './Ol.astro';
+export { default as Ul } from './Ul.astro';
