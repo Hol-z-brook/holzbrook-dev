3cd655556f84da1901f495b444576a39ae500b1d
Andrew Holbrook <holz55@gmail.com>
2025-03-06 14:56:59 -0700

    refactor: Update Andrew Holbrook's contact email

--- src/pages/work/_/candidate/andrewCandidate.data.ts
+++ src/pages/work/_/candidate/andrewCandidate.data.ts
@@ -9,7 +9,8 @@ export const andrewCandidate: WorkCandidate = {
   id: WorkCandidateId.AndrewHolbrook,
   name: 'Andrew Holbrook',
   title: 'Senior Frontend Engineer',
-  email: 'holz55@gmail.com',
+  // email: 'holz55@gmail.com',
+  email: 'andrew@holzbrook.dev',
   phone: '801.361.0417',
   summary:
     'Senior Software Engineer with 10+ years in front-end development. Specializes in React, Next.js, and Flutter, focusing on scalable, high-performance applications. Experienced in leading teams and driving product innovation.',
