5e3ad6b847d81c1ccd7dfdd46731e5b21cc200a0
Andrew Holbrook <holz55@gmail.com>
2025-03-05 18:03:54 -0700

    add alternative import method to /work/_/WorkPageSection

--- src/pages/work/_/WorkPageSection/index.ts
+++ src/pages/work/_/WorkPageSection/index.ts
@@ -2,6 +2,8 @@ import WorkPageSectionContainer from './WorkPageSectionContainer.astro';
 import WorkPageSectionGrid from './WorkPageSectionGrid.astro';
 import WorkPageSectionTitle from './WorkPageSectionTitle.astro';
 
+export { WorkPageSectionContainer, WorkPageSectionGrid, WorkPageSectionTitle };
+
 export default {
   Container: WorkPageSectionContainer,
   Grid: WorkPageSectionGrid,
