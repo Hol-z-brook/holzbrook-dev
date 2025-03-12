a66401e85976a3e337162d8f35c8302b315eff0c
Andrew Holbrook <holz55@gmail.com>
2025-03-12 11:34:29 -0600

    refactor: consolidate BusinessCard components in own directory

---
+++ src/pages/_/BusinessCard/index.ts
@@ -0,0 +1,13 @@
+import BusinessCardAvatar from './BusinessCardAvatar.astro';
+import BusinessCardContainer from './BusinessCardContainer.astro';
+import BusinessCardFullName from './BusinessCardFullName.astro';
+import BusinessCardJobTitle from './BusinessCardJobTitle.astro';
+import BusinessCardLink from './BusinessCardLink.astro';
+
+export default {
+  Avatar: BusinessCardAvatar,
+  Container: BusinessCardContainer,
+  FullName: BusinessCardFullName,
+  JobTitle: BusinessCardJobTitle,
+  Link: BusinessCardLink,
+};
