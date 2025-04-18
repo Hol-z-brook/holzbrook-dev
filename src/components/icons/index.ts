60b988f5b8df4437d7c7562c3da8fb00ec922465
Andrew Holbrook <holz55@gmail.com>
2025-04-18 14:55:36 -0600

    refactor: reorganize icon components and remove legacy icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+export type { IconProps } from './IconProps';
+export { default as EmailIcon } from './EmailIcon.astro';
+export { default as GitHubIcon } from './GitHubIcon.astro';
+export { default as LinkedInIcon } from './LinkedInIcon.astro';
+export { default as ResumeIcon } from './ResumeIcon.astro';
+export { default as DownloadIcon } from './DownloadIcon.astro';
+export { default as PrintIcon } from './PrintIcon.astro';
