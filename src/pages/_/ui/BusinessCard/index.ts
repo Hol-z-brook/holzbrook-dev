6c4a41783973d016f9415b55fd2be13a1cf6534e
Andrew Holbrook <holz55@gmail.com>
2025-04-18 20:09:44 -0600

    update home page business card

---
+++ src/pages/_/ui/BusinessCard/index.ts
@@ -0,0 +1,5 @@
+export { default as Container } from './BusinessCardContainer.astro';
+export { default as Avatar } from './BusinessCardAvatar.astro';
+export { default as FullName } from './BusinessCardFullName.astro';
+export { default as JobTitle } from './BusinessCardJobTitle.astro';
+export { default as Link } from './BusinessCardLink.astro';
